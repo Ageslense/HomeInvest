8ca02adafe5635f6e664ee4b27c39e420f4298bd
unknown <a.gunduz.er1998@gmail.com>
2020-07-26 13:54:42 +0300

    tablet resp

---
+++ assets/javascript/navbar.js
@@ -0,0 +1,15 @@
+let mainHench = {
+	toggleSidebar: function() {
+		let menuButton = document.querySelector( '.burger-menu' );
+		let navigationBar = document.getElementById( 'navbar-box' );
+
+		menuButton.addEventListener( 'click', function() {
+			navigationBar.classList.toggle( 'sidemenu-opened' );
+			document.getElementsByTagName( 'body' )[0].classList.toggle( 'no-scroll' );
+		});
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+	mainHench.toggleSidebar();
+})
